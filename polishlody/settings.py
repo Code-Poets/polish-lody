848e27f0017e29019286acc6c61439eb2c792109
Andrew Kysorets <andrzej.kysorets@o2.pl>
2016-09-20 13:43:09 +0200

    deleted a static files

--- polishlody/settings.py
+++ polishlody/settings.py
@@ -117,5 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = (os.path.join(os.path.dirname(__file__), '..', 'polishlody/static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- polishlody/urls.py
+++ polishlody/urls.py
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT,
-    }),
 ]
