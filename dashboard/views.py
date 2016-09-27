ed0f475f31988f06faa467a9e66e50ac2bf970c1
Andrew Kysorets <andrzej.kysorets@o2.pl>
2016-09-27 14:43:16 +0200

    Added a password change

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,4 +3,3 @@ from django.contrib.auth.decorators import login_required
 
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html', {})
-    
--- polishlody/urls.py
+++ polishlody/urls.py
@@ -17,11 +17,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
-
 # import ipdb; ipdb.set_trace()
+
 urlpatterns = [
     url(r'^$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^change-password/$', auth_views.password_change, {'template_name': 'registration/change-password.html'}, name='password_change'),
+    url(r'^change-password/done/$', auth_views.password_change_done, {'template_name': 'registration/change-password-done.html'}, name='password_change_done'),
     url(r'^admin/', admin.site.urls),
-    url(r'', include('dashboard.urls')),
+    url(r'^dashboard/', include('dashboard.urls')),
 ]
