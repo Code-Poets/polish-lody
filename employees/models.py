4e0844a2bc5fbb0ff20a05ea98fe750e4b9e10b2
Andrew Kysorets <andrzej.kysorets@o2.pl>
2016-10-18 15:36:40 +0200

    Changed the employee model to most valid

--- employees/models.py
+++ employees/models.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import validate_email
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=250, blank=True, default=None)
-    last_name = models.CharField(max_length=250, blank=True, default=None)
-    email = models.EmailField(max_length=75, blank=True, unique=True, default=None)
+    first_name = models.CharField(max_length=250, default=None)
+    last_name = models.CharField(max_length=250, default=None)
+    email = models.EmailField(validators=[validate_email], max_length=75, unique=True, default=None)
     rate_per_hour = models.DecimalField(max_digits=7, decimal_places=2, blank=True, default=0)
     contract_exp_date = models.DateField(blank=True, default=None, null=True)
     health_book_exp_date = models.DateField(blank=True, default=None, null=True)
