6d3afe25eafdb6d3c37f886509136816ce0ca753
Andrew Kysorets <andrzej.kysorets@o2.pl>
2016-10-19 12:42:36 +0200

    [M] Changed the employee model, added a single, combined migration

--- employees/models.py
+++ employees/models.py
@@ -4,9 +4,9 @@ from django.db import models
 from django.core.validators import validate_email
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=250, default=None)
-    last_name = models.CharField(max_length=250, default=None)
-    email = models.EmailField(validators=[validate_email], max_length=75, unique=True, default=None)
+    first_name = models.CharField(max_length=255, default=None)
+    last_name = models.CharField(max_length=255, default=None)
+    email = models.EmailField(validators=[validate_email], max_length=255, unique=True, default=None)
     rate_per_hour = models.DecimalField(max_digits=7, decimal_places=2, blank=True, default=0)
     contract_exp_date = models.DateField(blank=True, default=None, null=True)
     health_book_exp_date = models.DateField(blank=True, default=None, null=True)
