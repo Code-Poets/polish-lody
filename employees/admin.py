3958cadba82bb180561bd58108c612b92ef4d081
GW <g.weglinski@gmail.com>
2016-12-07 11:34:44 +0100

    updated admin.py to fit new models

--- employees/admin.py
+++ employees/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from .models import Employee, Month, Year
 
-from .models import Employee
+def make_month_paid(modeladmin, request, queryset):
+    queryset.update(salary_is_paid=True)
+
+def make_month_unpaid(modeladmin, request, queryset):
+    queryset.update(salary_is_paid=False)
+
+
+make_month_unpaid.short_description = "Mark selected months as unpaid"
+make_month_paid.short_description = "Mark selected months as paid"
+
+class MonthAdmin(admin.ModelAdmin):
+    actions = [make_month_paid, make_month_unpaid]
 
 admin.site.register(Employee)
+admin.site.register(Month, MonthAdmin)
+admin.site.register(Year)
+
