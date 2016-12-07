b270c21275dabfca8a380fa02bcd05d3ac084a2e
GW <g.weglinski@gmail.com>
2016-12-07 11:33:34 +0100

    additional fields in views.py

--- employees/views.py
+++ employees/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Employee
 
-
 class EmployeeList(LoginRequiredMixin, ListView):
     tmplate_name = 'employees/employee_list.html'
     context_object_name = 'all_employee_list'
@@ -19,15 +18,45 @@ class EmployeeDetail(LoginRequiredMixin, DetailView):
     model = Employee
     tmplate_name = 'employees/employee_detail.html'
 
+    def get_context_data(self, **kwargs):
+        #Overriding this method to use another model within the template
+        context = super(EmployeeDetail, self).get_context_data(**kwargs)
+        employee_object = self.get_object()
+        context['months'] = employee_object.month_set.all()
+        return context
+
+
 class EmployeeCreate(LoginRequiredMixin, CreateView):
+
     model = Employee
     success_url = reverse_lazy('employees')
-    fields = ['first_name', 'last_name', 'email', 'rate_per_hour', 'contract_exp_date', 'health_book_exp_date']
+    fields = ['first_name',
+              'last_name',
+              'email',
+              'gender',
+              'rate_per_hour',
+              'contract_start_date',
+              'contract_exp_date',
+              'health_book_exp_date',
+              'position',
+              'contract_type',
+              'hours_in_current_month']
+
 
 class EmployeeUpdate(LoginRequiredMixin, UpdateView):
     model = Employee
     success_url = reverse_lazy('employees')
-    fields = ['first_name', 'last_name', 'email', 'rate_per_hour', 'contract_exp_date', 'health_book_exp_date']
+    fields = ['first_name',
+              'last_name',
+              'email',
+              'gender',
+              'rate_per_hour',
+              'contract_start_date',
+              'contract_exp_date',
+              'health_book_exp_date',
+              'position',
+              'contract_type',
+              'hours_in_current_month']
 
 class EmployeeDelete(LoginRequiredMixin, DeleteView):
     model = Employee
