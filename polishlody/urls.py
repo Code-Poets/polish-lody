4eda7df9f579198623f5f10786fd236637600b85
Piotr Anusiewicz <yoowul@gmail.com>
2016-12-16 12:13:51 +0100

    Added work in progress on autologin decorator

--- polishlody/urls.py
+++ polishlody/urls.py
@@ -17,19 +17,20 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
-
-# import ipdb; ipdb.set_trace()
+from . import views
 
 urlpatterns = [
-    url(r'^$', auth_views.login,{'redirect_authenticated_user':True}, name='login'),
+    url(r'^$', auth_views.login,{'redirect_authenticated_user': True}, name='login'),
     url(r'^accounts/login/$', RedirectView.as_view(url='/')),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^change-password/$', auth_views.password_change, {'template_name': 'registration/change-password.html'}, name='password_change'),
     url(r'^change-password/done/$', auth_views.password_change_done, {'template_name': 'registration/change-password-done.html'}, name='password_change_done'),
-	url(r'^resetpassword/passwordsent/$', auth_views.password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),
+    url(r'^resetpassword/passwordsent/$', auth_views.password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),
     url(r'^resetpassword/$', auth_views.password_reset, {'template_name': 'registration/password_reset_form.html'}, name='password_reset'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name='password_reset_complete'),
+    #url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', views.password_reset_confirm, name='password_reset_confirm'),
+    #url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name='password_reset_complete'),
+    url(r'^reset/done/$', views.password_reset_complete, name='password_reset_complete'),
     url(r'^admin/', admin.site.urls),
     url(r'^dashboard/', include('dashboard.urls')),
     url(r'^employees/', include('employees.urls')),
