0fe7700003217265a4196106efc5455d655d75db
mandryl <g.weglinski@gmail.com>
2016-12-28 13:27:35 +0100

    additional tests for new features

--- employees/tests.py
+++ employees/tests.py
@@ -1,9 +1,13 @@
-from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from users.models import MyUser
+from django.test import TestCase, Client, RequestFactory
 from django.db import IntegrityError
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from datetime import datetime, timedelta, date
-from .models import Employee, Month, Year
+from .models import Employee, Month
+from employees import views
 
+User = get_user_model()
 class EmployeeMethodTests(TestCase):
     start_date = date.today()
     exp_date = date.today() + timedelta(days=30)
@@ -15,7 +19,7 @@ class EmployeeMethodTests(TestCase):
         exp_date = date.today() + timedelta(days=30)
         e = Employee(first_name='John', last_name='Gamlet', email="gamlet@example.com", contract_start_date=start_date,
             contract_exp_date=exp_date)
-        self.assertEqual(e.full_name(), "John Gamlet")
+        self.assertEqual(e.full_name(), "Gamlet John")
 
     def test_create_new_employee_with_empty_attributes(self):
         """
@@ -27,99 +31,13 @@ class EmployeeMethodTests(TestCase):
             Employee(first_name=None, last_name=None, email="gamlet.com", contract_start_date=start_date,
             contract_exp_date=exp_date).save()
 
-    fixtures = ['fixtures_user_and_months.json']
+    # fixtures = ['fixtures.json']
 
-    def test_should_calculating_salary_from_all_unpaid_months_work(self):
-        employee = Employee.objects.get(first_name='Britney')
-        all_months = employee.month_set.all()
-        sum_of_all_months = 0
-        for mon in all_months:
-            rph = mon.rate_per_hour_this_month
-            hpm = mon.hours_worked_in_this_month
-            sal = (rph * hpm)
-            sum_of_all_months += sal
-            sum_of_all_months -= mon.how_much_was_paid_to_employee
-        self.assertEqual(sum_of_all_months, 1750.00)
-        employee = Employee.objects.get(first_name='Czarny')
-        all_months = employee.month_set.all()
-        sum_of_all_months = 0
-        for mon in all_months:
-            rph = mon.rate_per_hour_this_month
-            hpm = mon.hours_worked_in_this_month
-            sal = (rph * hpm)
-            sum_of_all_months += sal
-            sum_of_all_months -= mon.how_much_was_paid_to_employee
-        self.assertEqual(sum_of_all_months, 0)
-
-    def test_should_save_method_create_appropriate_years_and_months_for_single_year(self):
-        start_date = date(2016, 3, 1)
-        exp_date = date(2016, 11, 1)
-        Employee(first_name="Alan", last_name="Shepard", email="asdf@mail.com", contract_start_date=start_date,
-                 contract_exp_date=exp_date).save()
-        employee = Employee.objects.get(first_name='Alan')
-        employee_months = employee.month_set.all()
-        employee_years = employee.year_set.all()
-        self.assertEqual(employee_months.count(), 9)
-        self.assertEqual(employee_months[0].simple_month_name(), 'March')
-        self.assertEqual(employee_months[8].simple_month_name(), 'November')
-        self.assertEqual(employee_months[2].simple_month_name(), 'May')
-        self.assertEqual(employee_years.count(), 1)
-        self.assertEqual(employee_years[0].year, '2016')
-
-    def test_should_save_method_not_be_so_bugged_anymore(self):
-        start_date = date(2016, 3, 1)
-        exp_date = date(2016, 6, 1)
-        Employee(first_name="Alan", last_name="Shepard", email="asdf@mail.com", contract_start_date=start_date,
-                 contract_exp_date=exp_date).save()
-        employee = Employee.objects.get(first_name='Alan')
-        new_start_date = date(2015, 10, 1)
-        employee.contract_start_date = new_start_date
-        employee.save()
-        employee_months = employee.month_set.all()
-        employee_years = employee.year_set.all()
-        self.assertEqual(employee_years.count(), 2)
-        self.assertEqual(employee_months.count(), 9)
-        new_start_date = date(2014, 10, 1)
-        new_exp_date = date(2015, 6, 1)
-        employee.contract_start_date = new_start_date
-        employee.contract_start_date = new_exp_date
-        employee_months = employee.month_set.all()
-        employee_years = employee.year_set.all()
-        self.assertEqual(employee_months.count(), 9)
-        self.assertEqual(employee_years.count(), 2)
-
-    def test_should_save_method_create_two_years_and_appropriate_months_for_two_years_of_work(self):
-        start_date = date(2183, 2, 1)
-        exp_date = date(2184, 11, 1)
-        employee = Employee(first_name="Alan", last_name="Shepard", email="asdf@mail.com",
-                            contract_start_date=start_date,
-                            contract_exp_date=exp_date,
-                            rate_per_hour='100'
-                            )
-        employee.save()
-        e = Employee.objects.get(first_name='Alan')
-        employee_months = e.month_set.all()
-        m = Month.objects.get(employee=e, month='January')
-        m.salary_is_paid = True
-        m.save()
-        employee_years = e.year_set.all()
-        self.assertEqual(employee_months.count(), 22)
-        self.assertEqual(employee_months[0].simple_month_name(), 'February')
-        self.assertEqual(employee_months[8].simple_month_name(), 'October')
-        self.assertEqual(employee_months[21].simple_month_name(), 'November')
-        self.assertEqual(employee_months[0].month_detail(), '2183 February')
-        self.assertTrue(employee_months[11].salary_is_paid) #Checks if month Jan 2184 is paid
-        self.assertEqual(employee_years.count(), 2)
-        self.assertEqual(employee_years[0].year, '2183')
-        self.assertEqual(employee_years[1].year, '2184')
-        ### Checks if default rate per hour is assigned for this month
-        self.assertEqual(str(employee_months[21].rate_per_hour_this_month), '100.00')
 
 
 class EmployeeListViewTests(TestCase):
 
     fixtures = ['user.json',]
-
     c = Client()
 
     def test_employee_list_view_with_no_employees(self):
@@ -161,11 +79,162 @@ class EmployeeListViewTests(TestCase):
             ['<Employee: Example Example>', '<Employee: Example2 Example2>']
         )
 
+class EmployeeDetailViewWithGrzesieksFixturesTests(TestCase):
+
+    fixtures = ['fixtures.json']
+    c = Client()
+
+    def test_should_employee_detail_view_show_correct_amount_of_months(self):
+        start_date = date.today()
+        exp_date = date.today() + timedelta(days=30)
+        employee = Employee.objects.create(first_name="Alan",
+                                           last_name="Shepard",
+                                           email="shepard@mail.com",
+                                           contract_start_date=start_date,
+                                           contract_exp_date=exp_date
+        )
+        employee.save()
+        self.c.login(username='manager@polishlody.pl',
+                     password='codepassword')
+
+        first_month = Month(
+            month=1,
+            year=2016,
+            employee=employee,
+        )
+        another_month = Month(
+            month=3,
+            year=2016,
+            employee=employee,
+            salary_is_paid=True,
+        )
+        another_month.save()
+        first_month.save()
+        url = reverse_lazy('employee_detail', args=(employee.id,))
+        response2 = self.c.get(url)
+        self.assertQuerysetEqual(
+            response2.context['months'],
+            ['<Month: 3 2016 - Alan Shepard - Paid!>','<Month: 1 2016 - Alan Shepard - Not paid yet.>']
+        )
+
+class MonthCreateViewTests(TestCase):
+
+    fixtures = ['fixtures.json']
+    c = Client()
+
+    def test_should_month_create_view_work(self):
+        self.c.login(username='manager@polishlody.pl',
+                     password='codepassword')
+        employee = Employee.objects.get(pk=10)
+        url = reverse_lazy('month_new', args=(employee.id,))
+        response = self.c.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['employee'], employee)
+        initial_form_data = response.context['form']
+        self.assertTrue('selected="selected">Alan Shepard<' in str(initial_form_data['employee']))
+        self.assertTrue('type="number" value="2016" required' in str(initial_form_data['year']))
+        self.assertTrue('type="number" value="%s" required' %
+                        (employee.rate_per_hour) in
+                        str(initial_form_data['rate_per_hour_this_month']))
+
+    def test_should_month_update_view_work(self):
+        self.c.login(username='manager@polishlody.pl',
+                     password='codepassword')
+        month = Month.objects.get(pk=30)
+        url = reverse_lazy('month_edit', args=(month.id,))
+        response = self.c.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['month'], month)
+        initial_form_data = response.context['form']
+        self.assertTrue('selected="selected">Czarny Lodziarz<' in str(initial_form_data['employee']))
+        self.assertTrue('type="number" value="2016" required' in str(initial_form_data['year']))
+        self.assertTrue('type="number" value="%s" required' %
+                        (month.rate_per_hour_this_month) in
+                        str(initial_form_data['rate_per_hour_this_month']))
+
+    def test_should_month_save_method_work_properly(self):
+        start_date = date(2184, 11, 7)
+        exp_date = date(2186, 11, 7,)
+        emp = Employee(first_name="Alan", last_name="Shepard", email="shepard@mail.com",
+                       contract_start_date=start_date,
+                      contract_exp_date=exp_date)
+        emp.save()
+        month_date = date(2016, 11, 7)
+        m = Month(year=2185, month=11, rate_per_hour_this_month=10,
+                  hours_worked_in_this_month=300, employee=emp)
+        m.save()
+        m2 = Month(year=2186, month=7, rate_per_hour_this_month=10,
+                   hours_worked_in_this_month=150, employee=emp, salary_is_paid=True)
+        m2.save()
+        self.assertEqual(emp.month_set.all().count(), 2)
+        mon = Month(year=month_date.year, month=month_date.month, rate_per_hour_this_month=10,
+                    hours_worked_in_this_month=300, employee=emp)
+        self.assertEqual(m.calculating_salary_for_this_month(), 3000)
+        self.assertEqual(m.month_name(), 'November')
+        self.assertEqual(m.month_detail(), '2185 November')
+        fail_month = Month(year=2185, month=11, rate_per_hour_this_month=10,
+                           hours_worked_in_this_month=300, employee=emp)
+        self.assertEqual(emp.all_unpaid_salaries(), 3000)
+        try:
+            fail_month.save()
+        except IntegrityError:
+            print("Unique together constraint raises IntegrityError as expected.")
+
+class MonthDeleteViewTests(TestCase):
+
+    fixtures = ['fixtures.json']
+    c = Client()
+
+    def test_should_month_delete_method_work_properly(self):
+        self.c.login(username='manager@polishlody.pl',
+                     password='codepassword')
+        start_date = date(2184, 11, 7)
+        exp_date = date(2186, 11, 7, )
+        emp = Employee(first_name="Alan", last_name="Shepard", email="shepard@mail.com",
+                       contract_start_date=start_date,
+                       contract_exp_date=exp_date)
+        emp.save()
+        m = Month(year=2185, month=11, rate_per_hour_this_month=10,
+                  hours_worked_in_this_month=300, employee=emp)
+        m.save()
+        m_id = m.id
+        response = self.c.get(reverse('month_delete', args=(m_id,)), follow=True)
+        self.assertTrue('Are you sure you want to delete' in str(response.content))
+        post_response = self.c.post(reverse('month_delete', args=(m.id,)), follow=True)
+        self.assertRedirects(post_response, reverse('employee_detail', args=(emp.id,)), status_code=302)
+        m.delete()
+        response404 = self.c.get(reverse('month_delete', args=(m_id,)), follow=True)
+        self.assertTrue('does not exist' in str(response404.content))
+
+class EmployeeDeleteViewTests(TestCase):
+
+    fixtures = ['fixtures.json']
+    c = Client()
+
+    def test_should_month_delete_method_work_properly(self):
+        self.c.login(username='manager@polishlody.pl',
+                     password='codepassword')
+        start_date = date(2184, 11, 7)
+        exp_date = date(2186, 11, 7, )
+        emp = Employee(first_name="Alan", last_name="Shepard", email="shepard@mail.com",
+                       contract_start_date=start_date,
+                       contract_exp_date=exp_date)
+        emp.save()
+        m = Month(year=2185, month=11, rate_per_hour_this_month=10,
+                  hours_worked_in_this_month=300, employee=emp)
+        m.save()
+        emp_id = emp.id
+        response = self.c.get(reverse('employee_delete', args=(emp.id,)), follow=True)
+        self.assertTrue('Are you sure you want to delete' in str(response.content))
+        post_response = self.c.post(reverse('month_delete', args=(m.id,)), follow=True)
+        self.assertRedirects(post_response, reverse('employee_detail', args=(emp.id,)), status_code=302)
+        emp.delete()
+        response404 = self.c.get(reverse('month_delete', args=(emp_id,)), follow=True)
+        self.assertTrue('does not exist' in str(response404.content))
 
 class EmployeeDetailViewTests(TestCase):
 
     fixtures = ['user.json',]
-
     c = Client()
 
     def test_employee_detail_view_with_the_existing_record(self):
@@ -184,7 +253,6 @@ class EmployeeDetailViewTests(TestCase):
         response = self.c.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "John")
-
     def test_employee_detail_view_with_no_existing_record(self):
         """
         the detail view of a no existing employee should be return status_code 404
