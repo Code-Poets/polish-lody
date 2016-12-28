2685a1fa0dc073f884905368354d60b3c1fc0cc2
mandryl <g.weglinski@gmail.com>
2016-12-28 13:36:18 +0100

    added forms based on respective models

---
+++ employees/forms.py
@@ -0,0 +1,35 @@
+from django.forms import ModelForm, RadioSelect
+from employees.models import Employee, Month
+from django.utils.safestring import mark_safe
+from django import forms
+from functools import partial
+
+dateinput = partial(forms.DateInput, {'class': 'datepicker'})
+
+# class DateRangeForm(forms.Form):
+#     start_date = forms.DateField(widget=DateInput())
+#     end_date = forms.DateField(widget=DateInput())
+
+class HorizontalRadioRenderer(RadioSelect.renderer):
+  def render(self):
+    return mark_safe(u'\n'.join([u'%s\n' % w for w in self]))
+
+class EmployeeForm(ModelForm):
+    class Meta:
+        model = Employee
+        exclude = []
+
+        widgets = {
+            'contract_start_date': forms.DateInput(attrs={'class':'datepicker'}),
+            'contract_exp_date': forms.DateInput(attrs={'class':'datepicker'}),
+            'health_book_exp_date': forms.DateInput(attrs={'class':'datepicker'}),
+        }
+
+class MonthForm(ModelForm):
+    class Meta:
+        model = Month
+        exclude = []
+
+        widgets = {
+            'salary_is_paid': RadioSelect(renderer=HorizontalRadioRenderer)
+        }
