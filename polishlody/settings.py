c56ffe879c69b04dc4a70a805c16b0c09d4f158c
Jakub89 <jakubczachura@gmail.com>
2017-01-02 11:23:45 +0100

    Debug true

--- polishlody/settings.py
+++ polishlody/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'niomi!^l#1doxv(%*#*-z73sb*ttz-rw@i+9)-inct7xex)zpp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.herokuapp.com']
 
