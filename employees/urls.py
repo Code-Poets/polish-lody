c30a6a8116a04ddc12a7c53201cbf1428e68d4f2
mandryl <g.weglinski@gmail.com>
2017-01-03 13:47:50 +0100

    pagination feature

--- employees/urls.py
+++ employees/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/new_month$', views.MonthCreate.as_view(template_name='employees/month_form.html'), name='month_new'),
     url(r'^delete/(?P<pk>\d+)$', views.EmployeeDelete.as_view(), name='employee_delete'),
     url(r'^del_month/(?P<pk>\d+)$', views.MonthDelete.as_view(), name='month_delete'),
+    url(r'^employees/per_page=(?P<per_page>[0-9]+)page=(?P<page>[0-9]+)/$', views.EmployeeList.as_view()),
+    url(r'^(?P<pk>\d+)/per_page=(?P<per_page>[0-9]+)page=(?P<page>[0-9]+)/$', views.EmployeeList.as_view()),
 ]
 
 handler404 = 'employees.views.pl_404_view'
