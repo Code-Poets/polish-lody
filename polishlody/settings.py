4574d3daec47f445862f63f0bcd99ffe03600862
mandryl <g.weglinski@gmail.com>
2017-01-17 14:25:37 +0100

    added context processor to settings variable.

--- polishlody/settings.py
+++ polishlody/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'employees.context_processors.getvars',
+                'employees.context_processors.getvars_perpage',
             ],
         },
     },
