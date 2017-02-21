29f2232d0efcd987e3a524f36b3c6907d789361a
mandryl <g.weglinski@gmail.com>
2017-02-21 17:46:03 +0100

    some sales data code and new design

--- sales/models.py
+++ sales/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
+import time
 # Create your models here.
 
 class Shop(models.Model):
@@ -13,39 +14,52 @@ class Shop(models.Model):
 
 class Date(models.Model):
 
-    date_shop = models.ForeignKey(Shop, null=True, blank=True, on_delete=models.CASCADE)
-    date = models.DateField(blank=True, default=timezone.now, null=True)
+    date = models.DateField(blank=True, default=timezone.now, null=True, unique=True)
 
     def __str__(self):
-        return str(self.date_shop) + ' ' + str(self.date)
+        return str(self.date)
 
-    class Meta:
-        unique_together = (("date", "date_shop"),)
+    def date_short(self):
+        return str(self.date.day) + '.' + str(self.date.month) + '.' + str(self.date.year)
+
+    def is_date_recent(self):
+        current_time = timezone.now()
+        expiration_date = self.date
+        # current_time = time.time()
+        expiration_date = int(time.mktime(time.strptime(str(expiration_date), '%Y-%m-%d')))
+        days_left = (expiration_date - current_time) / 259200
+        if days_left < 3:
+            return True
+        else:
+            return False
 
 class IceCream(models.Model):
 
-    icecream_shop = models.ForeignKey(Shop, null=True, blank=True, on_delete=models.CASCADE)
-    icecream_name = models.CharField(blank=True, null=True, default=None, max_length=64)
+    icecream_name = models.CharField(blank=True, null=True, default=None, max_length=64, unique=True)
+    icecream_standard_price = models.DecimalField(decimal_places=2, max_digits=10, default=0,
+                                         validators=[MinValueValidator(0)])
+    icecream_standard_production_cost = models.DecimalField(decimal_places=2, max_digits=8, default=0,
+                                                   validators=[MinValueValidator(0)])
 
     def __str__(self):
         return self.icecream_name
-
-    class Meta:
-        unique_together = (("icecream_shop", "icecream_name"),)
 
 class IceCreamCosts(models.Model):
 
     icecream = models.ForeignKey(IceCream, on_delete=models.CASCADE)
     icecream_shop = models.ForeignKey(Shop, null=True, blank=True, on_delete=models.CASCADE)
     icecream_date = models.ForeignKey(Date, null=True, blank=True, on_delete=models.CASCADE)
-    icecream_price = models.DecimalField(decimal_places=2, max_digits=10, default=0,
+    icecream_price = models.DecimalField(decimal_places=2, max_digits=8, default=0,
                                          validators=[MinValueValidator(0)])
-    icecream_production_cost = models.DecimalField(decimal_places=2, max_digits=10, default=0,
+    icecream_production_cost = models.DecimalField(decimal_places=2, max_digits=8, default=0,
                                                    validators=[MinValueValidator(0)])
     icecream_amount_sold = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return "Sold " + str(self.icecream_amount_sold) + " of this ice cream."
+        return str(self.icecream) + ' at ' + str(self.icecream_shop) + ': ' + str(self.icecream_date)
+
+    def icecream_revenue(self):
+        return self.icecream_amount_sold*self.icecream_price
 
     class Meta:
         unique_together = (("icecream_shop", "icecream", "icecream_date"),)
--- static/js/uncheck-filters.js
+++ static/js/uncheck-filters.js
@@ -1,7 +1,7 @@
-function uncheckFilters() {
-    $('input[type="checkbox"]:checked').prop('checked',false);
-    $('input:text').val('');
-    $('input:text').attr('placeholder', '');
-    $('.loading-icon').css('opacity', '1');
-    make_request();
-}
+// function uncheckFilters() {
+//     $('input[type="checkbox"]:checked').prop('checked',false);
+//     $('input:text').val('');
+//     $('input:text').attr('placeholder', '');
+//     $('.loading-icon').css('opacity', '1');
+//     make_request();
+// }
